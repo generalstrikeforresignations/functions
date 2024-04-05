e092be330221ca01705e493289fd70ee4961f61a
Beaei <info@codewhatever.us>
2024-04-05 04:15:21 -0500

    Removes object context from contact.

--- packages/serve/postContact/index.js
+++ packages/serve/postContact/index.js
@@ -4,10 +4,10 @@ async function main(args) {
     const uri = process.env['DATABASE_URL'];
     let client = new MongoClient(uri);
     console.log(args);
-    /*let new_contact = args;
+    let new_contact = args;
     try {
         await client.connect();
-        await client.db("knowledge").collection("contacts").insertOne({contact: new_contact});
+        await client.db("knowledge").collection("contacts").insertOne(new_contact);
         console.log(`added ${new_contact} to database.`);
         return { ok: true };
     } catch (e) {
@@ -18,7 +18,7 @@ async function main(args) {
         };
     } finally {
         await client.close();
-        }*/
+        }
     return {
         uri, args
     }
